b3dd8dbde5252a3137aae99a658b5aafea2f8d45
Scarlett Baez <s.baez1010@gmail.com>
2020-05-28 23:41:04 +0000

    Automatically backed up by Learn

--- flow-control.js
+++ flow-control.js
@@ -1,14 +1,21 @@
 function basicTeenager(age) {
+    if (age >= 13 && age <= 19) {
+      return "You are a teenager!";
+    }
+  }
 
-}
 
-function teenager(age) {
+  function teenager(age){
+    if (age >= 13 && age <= 19) {
+      return "You are a teenager!";
+    } else {
+      return "You are not a teenager";
+    }
+  }
 
-}
+function ageChecker(age){
 
-function ageChecker(age) {
 
-}
 
 function ternaryTeenager(age) {
 
